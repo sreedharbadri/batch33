9f84cfe84ecbaa207612919d66a364d27df9a5fb
sambapython@gmail.com <sambapython@gmail.com>
2017-08-19 08:58:16 +0530

    Modified statemtns in fun2

--- req2.py
+++ req2.py
@@ -2,6 +2,7 @@ def fun():
 	print "completed some part of req2"
 
 def fun2():
-    print "added by emp3"
+    print "added by emp3 sdsdfsd"
+    print "dsfsdfdsf"
 def fun3():
     print "added details by emp2"
